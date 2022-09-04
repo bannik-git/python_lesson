3af56f3d056558407f0368fddb232b6706c31701
Nikolay <nikolay_git@rambler.ru>
2022-09-04 12:36:30 +0500

    Исправлена ошибка

--- Homework/Seminar_1/001_Day_off.py
+++ Homework/Seminar_1/001_Day_off.py
@@ -1,4 +1,4 @@
-day_of_the_week = int(input("Введите порядковый номер дня недели: "))
+day_of_the_week = input("Введите порядковый номер дня недели: ")
 
 # можно сделать через цифры и лист [6, 7] или 6 <= day_of_the_week <= 7
 if day_of_the_week in '67': 
