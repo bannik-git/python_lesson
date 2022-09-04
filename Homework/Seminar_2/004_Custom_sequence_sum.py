ab20ebc2ae8b889e95f3353adce2add61a60bb57
Nikolay <nikolay_git@rambler.ru>
2022-09-04 19:49:43 +0500

    Задача на нахождение произведения элементов на указанных пользователем позициях

---
+++ Homework/Seminar_2/004_Custom_sequence_sum.py
@@ -0,0 +1,20 @@
+from random import randint
+
+user_number = int(input("Введите число: "))
+user_list = []
+
+for i in range(user_number):
+    user_list.append(randint(-user_number, user_number))
+
+print(f"\nВаша последовательность - {user_list}")
+
+user_index = input("""\nВведите индексы элементов произведение которых Вы хотите получить
+последовательность чисел вводить через пробел: """)
+
+index = user_index.split(" ")
+result = user_list[int(index[0])]
+
+for i in range(1, len(index)):
+    result *= user_list[int(index[i])]
+
+print(result)
