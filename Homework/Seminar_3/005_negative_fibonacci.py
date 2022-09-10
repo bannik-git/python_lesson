464a1f61484d7141cc721221b47b93e188d7d52a
Nikolay <nikolay_git@rambler.ru>
2022-09-11 01:12:37 +0500

    Задача на нахождение n элементов списка Фибоначчи (отрицательная и положительная часть)

---
+++ Homework/Seminar_3/005_negative_fibonacci.py
@@ -0,0 +1,26 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+# Пример:
+
+# - для k = 8 список будет выглядеть так: 
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+def negative_fibonacci(sequence_size: int) -> list:
+    
+    positive_sequence = [1, 1]
+    for i in range(2, sequence_size):
+        positive_sequence.append(positive_sequence[i-1] + positive_sequence[i-2])  
+
+    negative_sequence = positive_sequence.copy()
+    for i in range(len(negative_sequence)):
+        if i % 2 != 0:
+            negative_sequence[i] *= -1
+    negative_sequence.reverse()
+
+    return negative_sequence + [0] + positive_sequence
+
+amount_of_elements = int(input('Введите количество элементов: '))
+print(negative_fibonacci(amount_of_elements))
+
+
