e550bac0170c6e589a6d2209c33a741fd35a5291
Nikolay <nikolay_git@rambler.ru>
2022-09-10 22:15:22 +0500

    Задача на нахождение разницы между максимальным и минимальным значением дробной части элементов

---
+++ Homework/Seminar_3/003_diff_between_max_and_min_values_of_fractional_part.py
@@ -0,0 +1,20 @@
+# # Задайте список из вещественных чисел. Напишите программу, 
+# которая найдёт разницу между максимальным и минимальным 
+# значением дробной части элементов.
+
+# # Пример:
+
+# # - [1.1, 1.2, 3.1, 10.01] => 0.19
+
+
+def diff_between_max_and_min_values_of_fractional_part(number_list: list) -> float:
+    result_list = []
+    for i in number_list:
+        result_list.append(float('0.' + str(i).split(".")[1]))
+    
+    return max(result_list) - min(result_list) 
+
+
+user_number_list = [1.1, 1.2, 3.1, 10.01]
+result = diff_between_max_and_min_values_of_fractional_part(user_number_list)
+print(result)
