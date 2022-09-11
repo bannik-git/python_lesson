1f44a519973f47ecdc1d58c7eaee33098e509dbe
Nikolay <nikolay_git@rambler.ru>
2022-09-11 11:12:21 +0500

    Добавлен вариант исключения из расчета дробной части равной 0

--- Homework/Seminar_3/003_diff_between_max_and_min_values_of_fractional_part.py
+++ Homework/Seminar_3/003_diff_between_max_and_min_values_of_fractional_part.py
@@ -10,6 +10,7 @@
 def diff_between_max_and_min_values_of_fractional_part(number_list: list) -> float:
     result_list = []
     for i in number_list:
+        # if str(i).split(".")[1] != '0': Если надо исключить 0 из расчета
         result_list.append(float('0.' + str(i).split(".")[1]))
     
     return max(result_list) - min(result_list) 
